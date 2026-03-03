0cdde0641810c4ddc2c4fc02be61557fedf9c462
erickmjr <erickmellojr@gmail.com>
2026-03-02 21:13:14 -0300

    feat: cors options

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { json } from "express";
 import cors from 'cors';
 import router from "./routes";
+import cookieParser from 'cookie-parser';
 
 function createApp() {
     const app = express();
 
-    app.use(cors());
+    app.use(cookieParser());
+
+    app.use(cors({
+        origin: ['https://seu-frontend.onrender.com', 'http://localhost:5173'],
+        credentials: true
+    }));
+
     app.use(json());
 
     app.use('/api', router);
